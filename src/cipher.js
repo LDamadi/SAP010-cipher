8822c7d8316e85aceea8733b4baf326e40fb1d98
LDamadi <lrpdmarcenaria01@gmail.com>
2023-05-03 20:16:14 -0300

    logica cipher JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
-};
+  encode: function(desloc, texto){
+    if (typeof desloc !== 'number' || typeof texto !== 'string') //condição para ver se meu desloc é numero e se meu texto é string//
+    {throw new TypeError('os argumentos devem ser um number e uma string')}
+    else{
+      let textoCodificado = ""; 
+      for(let i = 0; i<texto.length; i++){ //Aqui inicia a =estrutura do laço de repetição for// 
+        let caracter = texto[i];
+        if(caracter.match(/[A-Z]/)){
+          const codigoAsc = texto.charCodeAt(i);
+          if(codigoAsc >= 65 && codigoAsc <= 90){
+            caracter = String.fromCharCode(((codigoAsc - 65 + desloc)% 26)+ 65); //Esta é a equação para criptografar e descriptografar//
+          }
+        }
+        textoCodificado += caracter;
+      }
+      return textoCodificado;
+    }
+
+
+  },
+  
+  decode: function(desloc, texto){
+    if (typeof desloc !== 'number' || typeof texto !== 'string') //condição para ver se meu desloc é numero e se meu texto é string//
+    {throw new TypeError('os argumentos devem ser um number e uma string')}
+    else{
+      let textoCodificado = ""; 
+      for(let i = 0; i<texto.length; i++){ //Aqui inicia a =estrutura do laço de repetição for// 
+        let caracter = texto[i];
+        if(caracter.match(/[A-Z]/)){
+          const codigoAsc = texto.charCodeAt(i);
+          if(codigoAsc >= 65 && codigoAsc <= 90){
+            let result = codigoAsc-65 - desloc;
+            while(result < 0){
+              result += 26;
+            }
+            caracter = String.fromCharCode((result % 26) + 65);
+          }
+        }
+        textoCodificado += caracter;
+      }
+      return textoCodificado;
+    }
+
+
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import cipher from './cipher.js';
-
-console.log(cipher);
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
-"q", "r", "s", "t", "u", "v", "x", "y", "w","z"]
-
-const button = document.getElementById("cifrar")
-const input = document.getElementById("texto")
-const inputDeslocamento = document.getElementById("offset")
-
-button.addEventListener("click", getInput)
-
-function getInput(event){
-    event.preventDefault()
-    console.log(input.value)
-    console.log(inputDeslocamento.value)
-    const deslocamento=  parseInt(inputDeslocamento.value)
-    console.log(typeof(deslocamento))
-    let senha = input.value
-    console.log(senha[0])
-    for (let i = 0; i < alfabeto.length; i++) {
-         if(senha[0] === alfabeto[i]){
-            console.log(alfabeto.length)
-            if(i+deslocamento>= alfabeto.length){
-                console.log(i+deslocamento)
-          senha=senha.replace(senha[0], alfabeto[deslocamento]) 
-          return console.log(senha) 
-            }
-            senha=senha.replace(senha[0], alfabeto[i + deslocamento])
-            return console.log(senha)
-        }
-      }
+console.log (cipher);
 
 
-}
+/*console.log(cipher);
+
+const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+    "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
+let senha = "windows";
+console.log("senha " + senha)
+console.log("primeira letra " + senha[0])
+let deslocamento = 3;
+console.log("deslocamento " + deslocamento)
+
+for (let posicaoDaLetraNoAlfabeto = 0; 
+    posicaoDaLetraNoAlfabeto < alfabeto.length; 
+    posicaoDaLetraNoAlfabeto++) {
+
+    if (senha[0] === alfabeto[posicaoDaLetraNoAlfabeto]) {
+     
+        //(codigoDaLetra + desloc) % tamanhoDoAlfabeto
+        let resultado = (posicaoDaLetraNoAlfabeto + deslocamento ) % alfabeto.length;
+        console.log("Posição da letra " + posicaoDaLetraNoAlfabeto)
+        console.log("Soma " + (posicaoDaLetraNoAlfabeto + deslocamento))
+        console.log("alfabeto " + alfabeto.length) 
+        console.log("Resultado " + resultado)
+        
+        senha = senha.replace(senha[0], alfabeto[resultado]);
+        return console.log(senha)
+        
+        
+    }//endIf
+    
+}//endFor*/
