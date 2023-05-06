76b0cd4fa04505fc1ebfcf57ec68923519a4a841
LDamadi <lrpdmarcenaria01@gmail.com>
2023-05-06 19:02:15 -0300

    Ajustes CSS

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
 import cipher from './cipher.js';
 console.log (cipher);
 
+document.getElementById("cifrar").addEventListener("click", myFunction);
 
-/*console.log(cipher);
+function myFunction(){
+const input = document.getElementById("cifrar").value;
 
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-    "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const encode = cipher.encode(input);
+document.getElementById("outputEncode").value=encode
+const decode = cipher.decode(input);
+document.getElementById("outputDecode").value=decode
 
+}
 
-let senha = "windows";
-console.log("senha " + senha)
-console.log("primeira letra " + senha[0])
-let deslocamento = 3;
-console.log("deslocamento " + deslocamento)
-
-for (let posicaoDaLetraNoAlfabeto = 0; 
-    posicaoDaLetraNoAlfabeto < alfabeto.length; 
-    posicaoDaLetraNoAlfabeto++) {
-
-    if (senha[0] === alfabeto[posicaoDaLetraNoAlfabeto]) {
-     
-        //(codigoDaLetra + desloc) % tamanhoDoAlfabeto
-        let resultado = (posicaoDaLetraNoAlfabeto + deslocamento ) % alfabeto.length;
-        console.log("Posição da letra " + posicaoDaLetraNoAlfabeto)
-        console.log("Soma " + (posicaoDaLetraNoAlfabeto + deslocamento))
-        console.log("alfabeto " + alfabeto.length) 
-        console.log("Resultado " + resultado)
-        
-        senha = senha.replace(senha[0], alfabeto[resultado]);
-        return console.log(senha)
-        
-        
-    }//endIf
-    
-}//endFor*/
