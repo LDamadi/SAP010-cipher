769fbdeda037bfe479c85d2ed53aecad1360e4e1
LDamadi <lrpdmarcenaria01@gmail.com>
2023-05-08 19:02:16 -0300

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-
-const button = document.getElementById("button")
-
-button.addEventListener("click", teste)
-
-function teste () {
-    console.log("chamou a funçao teste")
-    button.innerHTML = "criptografar"
+document.getElementById("cifrar").addEventListener("click", myFunction);
+function myFunction(){
+  const texto = document.getElementById("Digite").value; 
+  const offset = parseInt(document.getElementById("offset").value);
+  const mensagemCod = cipher.encode(offset, texto);
+  document.getElementById("outputEncode").value= mensagemCod;
 
 }
+document.getElementById("decifrar").addEventListener("click", descriptografar);
+function descriptografar(){
+  const texto = document.getElementById("outputEncode").value; 
+  const offset = parseInt(document.getElementById("offset").value);
+  const descript = cipher.decode(offset, texto);
+  document.getElementById("outputDecode").value=descript;
 
+}
 
